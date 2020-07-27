c916c019da59f4fb1c30ec9cf27c9b880cb62623
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2020-07-28 00:59:26 +0100

    added kanban board styling

--- job_tracker_project/settings.py
+++ job_tracker_project/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'applications_list'
