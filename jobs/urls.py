3b1d42106df41eb5e7fc212af71bb9e63ce0a375
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2020-07-29 15:18:49 +0100

    changed styling and added job search page

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import all_applications_view, ApplicationCreateView, ApplicationDeleteView, ApplicationUpdateView
+from .views import all_applications_view, job_results_view, ApplicationCreateView, ApplicationDeleteView, ApplicationUpdateView
 
 urlpatterns = [
     path('', all_applications_view, name='applications_list'),
+    path('search-results/', job_results_view, name='job_results'),
     path('new/', ApplicationCreateView.as_view(), name='application_new'),
-    path('<int:pk>/edit', ApplicationUpdateView.as_view(), name='application_edit'),
+    path('<int:pk>/detail', ApplicationUpdateView.as_view(), name='application_detail'),
     path('<int:pk>/delete', ApplicationDeleteView.as_view(), name='application_delete'),
 ]
