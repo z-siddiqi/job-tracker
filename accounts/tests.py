94796285fa0e6fc4247fd2350bac0340a07bd6b3
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2020-10-23 17:35:48 +0100

    added tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,28 +3,46 @@ from django.test import TestCase
 from django.urls import reverse
 
 # Create your tests here.
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='testuser',
+            age=23,
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.age, 23)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        super_user = User.objects.create_superuser(
+            username='superuser',
+            email='superuser@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(super_user.username, 'superuser')
+        self.assertEqual(super_user.email, 'superuser@email.com')
+        self.assertTrue(super_user.is_superuser)
+
+
 class SignupPageTests(TestCase):
 
-    username = 'newuser'
-    email = 'newuser@email.com'
+    def setUp(self):
+        self.username = 'testuser'
+        self.age = 23
+        self.response = self.client.get(reverse('signup'))
 
-    def test_signup_page_status_code(self):
-        response = self.client.get('/accounts/signup/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'signup.html')
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
 
     def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(self.username, self.email)
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()
-                         [0].username, self.username)
-        self.assertEqual(get_user_model().objects.all()
-                         [0].email, self.email)
+        User = get_user_model()
+        new_user = User.objects.create_user(username=self.username, age=self.age)
+        self.assertEqual(User.objects.all().count(), 1)
+        self.assertEqual(User.objects.all()[0].username, self.username)
+        self.assertEqual(User.objects.all()[0].age, self.age)
