eef241565a9074f574a1a21eb925930201cc6c64
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-04-23 17:29:05 +0100

    set up hybrid django react architecture

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './static/build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            [
+              "@babel/preset-react",
+              { "runtime": "automatic" }
+            ]
+          ]
+        }
+      },
+    ]
+  }
+};
