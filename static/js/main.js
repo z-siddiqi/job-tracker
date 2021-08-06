883a27247b4e174f7f3e8d082348d720a290ed97
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2021-08-06 16:41:56 +0100

    refactor all js

--- static/js/main.js
+++ static/js/main.js
@@ -1,162 +1,107 @@
-// modal
-var showModal = function () {
-	var btn = $(this);
-	var modalId = btn.data('modal');
-	$.ajax({
-		url: btn.data('url'),
-		type: 'get',
-		dataType: 'json',
-		success: function (response) {
-			if (response.status != 403) {
-				$(modalId + " .modal-content").html(response.form);
-				$(modalId).modal('show');
-			}
-		}
-	});
-	return false;
-}
+const fetchModalContent = async function (url) {
+  const requestOptions = {
+    method: "GET",
+    headers: { "X-Requested-With": "XMLHttpRequest" },
+  };
+  const response = await fetch(url, requestOptions);
+  const data = await response.json();
+  return data.form;
+};
 
-var saveModalForm = function () {
-	var form = $(this);
-	$.ajax({
-		url: form.attr('action'),
-		data: form.serialize(),
-		type: 'post',
-		dataType: 'json',
-		success: function (response) {
-			if (response.form) {
-				// form invalid
-				form.closest('.modal-content').html(response.form);
-			} else if (response.status == 302) {
-				// form valid with redirect
-				window.location.href = response.url;
-			} else if (response.status == 200) {
-				// form valid without redirect
-				if (response.task) {
-					// add task
-					appendTask(response.task);
-					form[0].reset();
-				} else if (response.board) {
-					// edit board title
-					$("#boardTitle h3").text(response.board.title)
-					form.closest('.modal').modal('toggle');
-				} else {
-					// reload jobs
-					loadJobs();
-					form.closest('.modal').modal('toggle');
-				}
-			}
-		}
-	});
-	return false;
-}
+const submitForm = async function (form) {
+  const url = form.action;
+  const formData = new FormData(form);
+  const requestOptions = {
+    method: "POST",
+    headers: { "X-Requested-With": "XMLHttpRequest" },
+    body: formData,
+  };
+  const response = await fetch(url, requestOptions);
+  const data = await response.json();
+  return data;
+};
 
-var clearModal = function () {
-	$(this).find('.modal-content').empty();
-}
+const setInnerHTML = function (elm, html) {
+  elm.innerHTML = html;
+  Array.from(elm.querySelectorAll("script")).forEach((oldScript) => {
+    const newScript = document.createElement("script");
+    Array.from(oldScript.attributes).forEach((attr) =>
+      newScript.setAttribute(attr.name, attr.value)
+    );
+    newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+    oldScript.parentNode.replaceChild(newScript, oldScript);
+  });
+};
 
-// job
-var loadJobs = function () {
-	var url = $("#boardButtons .board-edit").data('url').replace('edit', 'jobs')
-	$.ajax({
-		url: url,
-		type: 'get',
-		dataType: 'json',
-		success: function (response) {
-			$("#applied, #phone, #onsite, #offer").empty();
-			$.each(response, function (index, val) {
-				appendJob(val);
-			});
-		}
-	});
-	return false;
-}
+const initSummernote = function () {
+  const origin = document.querySelector("textarea.summernoteinplacewidget");
+  const settings = JSON.parse(
+    document.querySelector("#summernoteSettings").textContent
+  );
+  const additionalSettings = {
+    callbacks: {
+      // remove formatting on paste
+      onPaste: function (e) {
+        e.preventDefault();
+        const bufferText = (
+          (e.originalEvent || e).clipboardData || window.clipboardData
+        ).getData("Text");
+        setTimeout(function () {
+          document.execCommand("insertText", false, bufferText);
+        }, 10);
+      },
+    },
+  };
+  $(origin).summernote({ ...settings, ...additionalSettings });
+};
 
-var scrapeJob = function () {
-	var csrfToken = $("input[name=csrfmiddlewaretoken]").val();
-	var inputUrl = $("#inputUrl").val();
-	$.ajax({
-		url: $(this).data('url'),
-		data: {
-			csrfmiddlewaretoken: csrfToken,
-			jobUrl: inputUrl
-		},
-		type: 'post',
-		success: function (response) {
-			$("#id_company").val(response.company);
-			$("#id_title").val(response.title);
-			$("#id_description").summernote('code', response.description);
-		}
-	});
-}
+const showModal = async function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  const modalId = e.currentTarget.dataset.modal;
+  const url = e.currentTarget.href ?? e.currentTarget.dataset.url;
+  fetchModalContent(url)
+    .then((content) => {
+      const modalEl = document.querySelector(modalId);
+      const modalContent = modalEl.querySelector(".modal-content");
+      setInnerHTML(modalContent, content);
+      const modal = bootstrap.Modal.getOrCreateInstance(modalEl);
+      modal.show();
+    })
+    .catch((error) => console.log(error));
+};
 
-var appendJob = function (job) {
-	const markup = `
-			<div class="card btm-shadow mb-2 show-modal" data-modal="#largeModal" data-url="/boards/${job.board_slug}/jobs/${job.slug}/edit">
-				<div class="card-body p-3 rounded cursor-pointer grey-hover">
-					<div class="row no-gutters">
-						<div class="col-11">
-							<h5 class="mr-3">${job.company}</h5>
-						</div>
-						<div class="col-1">
-							<a href="#" class="float-right show-modal" data-modal="#smallModal" data-url="/boards/${job.board_slug}/jobs/${job.slug}/delete">
-								<i class="bx bx-trash" id="icon"></i>
-							</a>
-						</div>
-					</div>
-					<h5 class="font-weight-bold">${job.title}</h5>
-					<p class="text-muted mb-0">${job.deadline}</p>
-				</div>
-			</div>
-		`;
-	$("#" + job.progress).append(markup);
-}
+const renderJobs = function () {
+  const jobTemplates = state.jobs.reduce(function (result, job) {
+    if (!result[job.progress]) {
+      result[job.progress] = [];
+    }
+    result[job.progress].push(jobTemplate(job));
+    return result;
+  }, {});
 
-// task
-var appendTask = function (task) {
-	const markup = `
-			<div class="card mb-1" id="taskCard" data-id="${task.id}">
-				<div class="card-body">
-					<div class="form-check-inline mw-85 overflow-hidden">
-						<input class="form-check-input float-left task-complete" type="checkbox">
-						<label class="form-check-label">
-							${task.task}
-						</label>
-					</div>
-					<button type="button" class="close float-right task-delete">
-						<span aria-hidden="true"><i class="bx bxs-x-square"></i></span>
-					</button>
-				</div>
-			</div>
-		`;
-	$("#taskList").append(markup);
-}
+  for (const [progress, templates] of Object.entries(jobTemplates)) {
+    document.querySelector(`#${progress}`).innerHTML = templates.join("");
+  }
+};
 
-var completeTask = function () {
-	var csrfToken = $("input[name=csrfmiddlewaretoken]").val();
-	var dataID = $(this).closest('div.card').data('id');
-	$.ajax({
-		url: '/tasks/' + dataID + '/complete/',
-		data: {
-			csrfmiddlewaretoken: csrfToken,
-			id: dataID
-		},
-		type: 'post'
-	});
-}
-
-var deleteTask = function () {
-	var csrfToken = $("input[name=csrfmiddlewaretoken]").val();
-	var dataID = $(this).closest('div.card').data('id');
-	$.ajax({
-		url: '/tasks/' + dataID + '/delete/',
-		data: {
-			csrfmiddlewaretoken: csrfToken,
-			id: dataID
-		},
-		type: 'post',
-		success: function () {
-			$('#taskCard[data-id="' + dataID + '"]').remove();
-		}
-	});
-}
+const jobTemplate = function (job) {
+  return `
+			  <div class="card btm-shadow cursor-pointer grey-hover mb-2" data-slug="${job.slug}" data-modal="#largeModal" data-url="/boards/${job.board_slug}/jobs/${job.slug}/edit" onclick="showModal(event)">
+				  <div class="card-body p-3 rounded">
+					  <div class="row g-0">
+						  <div class="col-11">
+							  <h5 class="me-3">${job.company}</h5>
+						  </div>
+						  <div class="col-1">
+							  <a href="/boards/${job.board_slug}/jobs/${job.slug}/delete" class="float-end" data-modal="#smallModal" onclick="showModal(event)">
+				          <i class="bi bi-trash fs-5"></i>
+							  </a>
+						  </div>
+					  </div>
+					  <h5 class="fw-bold">${job.title}</h5>
+					  <p class="text-muted mb-0">${job.deadline}</p>
+				  </div>
+			  </div>
+		  `;
+};
